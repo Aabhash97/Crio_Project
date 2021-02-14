fb40fbfd2dba1ce1834f05c6bd835c627ff7c056
Aabhash Tiwari <35108630+Aabhash97@users.noreply.github.com>
2021-02-15 00:58:01 +0530

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 cd xmeme
-sudo mvn clean install -DskipTests
-
+sudo ./mvnw clean install
 sudo java -jar target/xmeme-0.0.1-SNAPSHOT.jar &
 
 
-while ! netstat -tna | grep 'LISTEN\>' | grep -q ':8081\>'; do
-  echo "waiting for spring application to start"
-  sleep 2 # time in seconds, tune it as needed
-done
 
 
