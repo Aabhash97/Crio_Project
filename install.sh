4102fa4e25ade56def63a83661cd1e9442db5b16
Aabhash97 <abhashtiwari.97@gmail.com>
2021-02-14 23:33:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install -y postgresql
+sudo apt-get update
+sudo apt-get -y install postgresql
 sudo -u postgres psql postgres
 -u postgres psql --command '\password postgres'
 \q
 
-sudo apt install -y openjdk-11-jre-headless
+sudo apt update
+sudo apt install -y install default-jre
 
