016fa2a9023cb1324df9f1c97990aa693d0888e0
Aabhash Tiwari <35108630+Aabhash97@users.noreply.github.com>
2021-02-15 01:01:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get -y install postgresql
 sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'password';"
 
-sudo apt update
+sudo apt-update
 sudo apt install -y install default-jre
-sudo apt - y install maven
+sudo apt -y install maven
 
